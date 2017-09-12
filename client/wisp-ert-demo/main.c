38b4afc9c49cd787eb8db9bcd6e4866fea9f9b85
Qifan Lu <lqf.1996121@gmail.com>
2017-09-12 12:26:31 -0700

    Demo read code update

--- client/wisp-ert-demo/main.c
+++ client/wisp-ert-demo/main.c
@@ -5,19 +5,23 @@
 void ert_main() {
     //Status
     ert_status_t status;
+
     //File descriptor
     int16_t fd;
     //Placeholder variable
     int16_t _;
+    //Read data
+    uint8_t* read_data;
 
     //Open a file
+    //ERT_AWAIT(status, int16_t, fd, ert_open, "./test.txt", O_RDONLY, 0644)
     ERT_AWAIT(status, int16_t, fd, ert_open, "./test.txt", O_CREAT|O_RDWR, 0644)
     if (status!=0)
         goto demo_end;
 
     //Read file
     /*
-    ERT_AWAIT(status, int16_t, _, ert_read, fd, 5)
+    ERT_AWAIT(status, uint8_t*, read_data, ert_read, fd, 5)
     if (status!=0)
         goto demo_end;
     */
