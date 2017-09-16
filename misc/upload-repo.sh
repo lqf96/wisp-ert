afd840f52d4f7b44d5f416ebbfe8a27a33b64a57
Qifan Lu <lqf.1996121@gmail.com>
2017-09-16 15:46:51 +0800

    Related repo and wiki repo profile

---
+++ misc/upload-repo.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+
+# Change directory
+cd "$(dirname $0)"
+# Configuration file
+if [ -n "${CONFIG}" ]; then
+    source "${CONFIG}.sh"
+else
+    echo "Error: Please pass configuration script by setting CONFIG variable!" >&2
+    exit 1
+fi
+
+# Before uplink hook
+before_upload_hook
+
+# Create temporary directory
+_TMP_REPO_DIR="$(mktemp -d)"
+# Clone repository into temporary directory
+git clone "${REPO_URL}" -b "${REPO_BRANCH}" "${_TMP_REPO_DIR}"
+# Copy all files from source directory to repository
+cp -a "${SOURCE_DIR}"/* "${_TMP_REPO_DIR}"
+
+# Get reporitory version information
+_REPO_VERSION_INFO="$(git describe --tags)"
+# Add changes
+git -C "${_TMP_REPO_DIR}" add .
+# Commit changes
+git -C "${_TMP_REPO_DIR}" commit -m "${_REPO_VERSION_INFO}"
+# Push commit to remote
+git -C "${_TMP_REPO_DIR}" push origin "${REPO_BRANCH}"
+
+# Remove temporary directory
+rm -rf "${_TMP_REPO_DIR}"
+
+# After upload hook
+after_upload_hook
---
+++ misc/wiki.sh
@@ -0,0 +1,16 @@
+# Repository URL
+REPO_URL="ssh://github.com/lqf96/wisp-ert.wiki.git"
+# Repository branch
+REPO_BRANCH="master"
+
+# Source
+SOURCE_DIR="../wiki"
+
+# Before upload hook
+before_upload_hook() {
+    :
+}
+# After upload hook
+after_upload_hook() {
+    :
+}
